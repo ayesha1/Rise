14ee7fe6cd2b1a0d7798a84376aaf9b8d80e1b6a
Ayesha Khan <akhan1231@hotmail.com>
2019-08-10 20:44:04 -0700

    Icons Added

---
+++ Rise/ChatViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.swift
+//  Rise
+//
+//  Created by Ayesha Khan on 8/10/19.
+//  Copyright © 2019 Ayesha Khan. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+class ChatViewController: AVPlayerViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
